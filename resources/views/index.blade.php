0aa8e393797ad5521fe56c3cdae4d3e877734857
shtoorman <bar2683@mail.ru>
2018-02-27 19:14:54 +0200

    addproject

---
+++ resources/views/index.blade.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+    <link rel="shortcut icon" href="../../public/images/favicon.ico" type="image/x-icon">
+    <title>Сайт визика Баландина Александра</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="css/fancybox.css"/>
+    <link rel="stylesheet" type="text/css"
+          href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,300,800,700,400italic|PT+Serif:400,400italic"/>
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easytabs.min.js"></script>
+    <script type="text/javascript" src="js/respond.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easytabs.min.js"></script>
+    <script type="text/javascript" src="js/jquery.adipoli.min.js"></script>
+    <script type="text/javascript" src="js/jquery.fancybox-1.3.4.pack.js"></script>
+    <script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="js/jquery.gmap.min.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+
+</head>
+<body>
+<!-- Container -->
+<div id="container">
+
+    <!-- Top -->
+    <div class="top">
+        <!-- Logo -->
+        <div id="logo">
+            <h2>Баландин Александр</h2>
+            <h4>WEB DEWELOPER</h4>
+        </div>
+        <!-- /Logo -->
+
+        <!-- Social Icons -->
+        <ul class="socialicons">
+            <li><a href="#" class="social-text">Я в соц. сетях</a></li>
+            <li><a href="https://www.facebook.com/shtoormanit" target="_blank" class="social-facebook"></a></li>
+            <li><a href="https://plus.google.com/u/0/115092972555167903407" target="_blank" class="social-twitter"></a>
+            </li>
+            <li><a href="https://www.instagram.com/bar2683/" target="_blank" class="social-googleplus"></a></li>
+        </ul>
+        <!-- /Social Icons -->
+    </div>
+    <!-- /Top -->
+
+    <!-- Content -->
+    <div id="content">
+
+        <!-- Profile -->
+        <div id="profile">
+            <!-- About section -->
+            <div class="about">
+                <div class="photo-inner"><img src="images/photo.jpg" height="186" width="153"/></div>
+                <h1>Balandin Aleksandr</h1>
+                <h3>WEB DEWELOPER</h3>
+                <p>Я люблю заниматся программированием. Создавать игры, сайты. Это мое увлечение.</p>
+            </div>
+            <!-- /About section -->
+
+            <!-- Personal info section -->
+            <ul class="personal-info">
+                <li><label>Имя</label><span>Баландин Александр</span></li>
+                <li><label>Дата рождения</label><span>26 июля 1983</span></li>
+                <li><label>Адрес</label><span>г.Сумы 40035 Украина</span></li>
+                <li><label>Email</label><span>shtoorman2683@gmail.com</span></li>
+                <li><label>Телефон</label><span>+380997714691</span></li>
+                <li><label>Website</label><span><a href="http://shtoorman.mooo.com/" target="_blank">http://shtoorman.mooo.com/</a> </span></li>"
+            </ul>
+            <!-- /Personal info section -->
+        </div>
+        <!-- /Profile -->
+
+        <!-- Menu -->
+        <div class="menu">
+            <ul class="tabs">
+                <li><a href="#profile" class="tab-profile">Профиль</a></li>
+                <li><a href="#resume" class="tab-resume">Резюме</a></li>
+                <li><a href="#portfolio" class="tab-portfolio">Портфолио</a></li>
+                <li><a href="#contact" class="tab-contact">Контакты</a></li>
+            </ul>
+        </div>
+        <!-- /Menu -->
+
+        <!-- Resume -->
+        <div id="resume">
+            <div class="timeline-section">
+                <!-- Timeline for Employment  -->
+                <h3 class="main-heading"><span>Мое развитие</span></h3>
+                <ul class="timeline">
+                    <li>
+
+                        <div class="timelineUnit">
+                            <h4>Информационные технологии<span class="timelineDate">2013</span></h4>
+                            <h5>Бакалавр</h5>
+                            <p>Закончил СумДу в 2013 году. Получил образование бакалавр информационных технологий.</p>
+                        </div>
+                    </li>
+                    <li>
+
+                        <div class="timelineUnit">
+                            <h4>Python Developer<span class="timelineDate">2013-2014</span></h4>
+                            <h5>Питон или Пайтон</h5>
+                            <p>Занимался разработкой приложений, созданием игр на Python с помощью библиотеки
+                                Tkinter.</p>
+                        </div>
+                    </li>
+                    <li>
+
+                        <div class="timelineUnit">
+                            <h4>Java Developer<span class="timelineDate">2015</span></h4>
+                            <h5>Джава в честь кофе))) </h5>
+                            <p title="Нажмите чтоб перейти." ><a href="http://sumdu.edu.ua/ru/netcracker" target="_blank">
+                                    Самообразование и курсы. Курсы тестеровщика java от компании Netcracker в СумДу.</a></p>
+                        </div>
+                    </li>
+                    <div class="clear"></div>
+                </ul>
+                <!-- /Timeline for Employment  -->
+
+                <!-- Timeline for Education  -->
+                <!--h3 class="main-heading"><span>Education</span></h3-->
+                <ul class="timeline">
+                    <li>
+                        <div class="timelineUnit">
+                            <h4>PHP Developer<span class="timelineDate">2016-2017</span></h4>
+                            <h5>Single Page Applications</h5>
+                            <p title="Нажмите чтоб перейти."><a href="http://www.learn.mindk.com/" target="_blank">Курсы на языке PHP программирования, для создания
+                                фреймворков и web приложений.</a></p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="timelineUnit">
+                            <h4>laravel<span class="timelineDate">2017</span></h4>
+                            <h5>Создание сайтов на фреймворке laravel</h5>
+                            <p title="Нажмите чтобы перейти."><a htef="https://laravel.com/" terget="_blank">
+                                    Создание и продвижение сайтов на фреймворке laravel.</a></p>
+                        </div>
+                    </li>
+                    <div class="clear"></div>
+                </ul>
+                <!-- /Timeline for Education  -->
+            </div>
+            <div class="skills-section">
+                <!-- Skills -->
+                <h3 class="main-heading"><span>Graphics and media skills</span></h3>
+                <ul class="skills">
+                    <li>
+                        <h4>Photoshop</h4>
+                        <span class="rat6"></span>
+                    </li>
+                    <li>
+                        <h4>Paint</h4>
+                        <span class="rat4"></span>
+                    </li>
+                    <li>
+                        <h4>Flash</h4>
+                        <span class="rat4"></span>
+                    </li>
+                    <li>
+                        <h4>Corel Draw</h4>
+                        <span class="rat2"></span>
+                    </li>
+
+                <h3 class="main-heading"><span>Programming Skills</span></h3>
+                <ul class="skills">
+                    <li>
+                        <h4>Python</h4>
+                        <span class="rat6"></span>
+                    </li>
+                    <li>
+                        <h4>Php/Sql</h4>
+                        <span class="rat6"></span>
+                    </li>
+                    <li>
+                        <h4>Javascript</h4>
+                        <span class="rat3"></span>
+                    </li>
+                    <li>
+                        <h4>HTML</h4>
+                        <span class="rat4"></span>
+                    </li>
+                    <li>
+                        <h4>CSS</h4>
+                        <span class="rat4"></span>
+                    </li>
+                    <li>
+                    <h4>Git</h4>
+                    <span class="rat5"></span>
+                    </li>
+                    <li>
+                        <h4>Composer</h4>
+                        <span class="rat7"></span>
+                    </li>
+                </ul>
+                <h3 class="main-heading"><span>Design Skills</span></h3>
+                <ul class="skills">
+                    <li>
+                        <h4>Graphic Design</h4>
+                        <span class="rat4"></span>
+                    </li>
+                    <li>
+                        <h4>Web design</h4>
+                        <span class="rat3"></span>
+                    </li>
+                    <li>
+                        <h4>Print Design</h4>
+                        <span class="rat6"></span>
+                    </li>
+                </ul>
+                <!-- /Skills -->
+            </div>
+        </div>
+        <!-- /Resume -->
+
+        <!-- Portfolio -->
+        <div id="portfolio">
+
+            <ul id="portfolio-filter">
+                <li><a href="#" class="current" data-filter="*">All</a></li>
+                <li><a href="#" data-filter=".webdesign">WebDesign</a></li>
+                <li><a href="#" data-filter=".photoghraphy">Photoghraphy</a></li>
+                <li><a href="#" data-filter=".illustration">Illustration</a></li>
+                <li><a href="#" data-filter=".print">Print</a></li>
+                <li><a href="#" data-filter=".animation">Animation</a></li>
+            </ul>
+            <div class="extra-text">Some of the projects i'm proud with</div>
+            <ul id="portfolio-list">
+                <li class="illustration print">
+                    <a href="portfolio/1.jpg" rel="portfolio">
+                        <img src="portfolio/1-thumb.jpg" alt="">
+                        <h2 class="title">Hello Goodbye</h2>
+                        <span class="categorie">Print / Illustration</span>
+                    </a>
+                </li>
+                <li class="photoghraphy">
+                    <a href="portfolio/2.jpg" rel="portfolio">
+                        <img src="portfolio/2-thumb.jpg" alt="">
+                        <h2 class="title">Help us</h2>
+                        <span class="categorie">Photography</span>
+                    </a>
+                </li>
+                <li class="webdesign">
+                    <a href="portfolio/3.jpg" rel="portfolio">
+                        <img src="portfolio/3-thumb.jpg" alt="">
+                        <h2 class="title">Snow time</h2>
+                        <span class="categorie">Webdesign</span>
+                    </a>
+                </li>
+                <li class="animation print">
+                    <a href="portfolio/4.jpg" rel="portfolio">
+                        <img src="portfolio/4-thumb.jpg" alt="">
+                        <h2 class="title">Sunshine</h2>
+                        <span class="categorie">Print / Animation</span>
+                    </a>
+                </li>
+                <li class="animation">
+                    <a href="portfolio/5.jpg" rel="portfolio">
+                        <img src="portfolio/5-thumb.jpg" alt="">
+                        <h2 class="title">Come together</h2>
+                        <span class="categorie">Animation</span>
+                    </a>
+                </li>
+                <li class="webdesign print">
+                    <a href="portfolio/6.jpg" rel="portfolio">
+                        <img src="portfolio/6-thumb.jpg" alt="">
+                        <h2 class="title">The yellow</h2>
+                        <span class="categorie">Webdesign / Print</span>
+                    </a>
+                </li>
+                <li class="photoghraphy">
+                    <a href="portfolio/7.jpg" rel="portfolio">
+                        <img src="portfolio/7-thumb.jpg" alt="">
+                        <h2 class="title">Black bird</h2>
+                        <span class="categorie">Photoghraphy</span>
+                    </a>
+                </li>
+                <li class="illustration">
+                    <a href="portfolio/8.jpg" rel="portfolio">
+                        <img src="portfolio/8-thumb.jpg" alt="">
+                        <h2 class="title">Revolution</h2>
+                        <span class="categorie">Illustration</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- /Portfolio -->
+
+        <!-- Contact -->
+        <div id="contact">
+            <div id="map"></div>
+            <!-- Contact Info -->
+            <div class="contact-info">
+                <h3 class="main-heading"><span>Contact info</span></h3>
+                <ul>
+                    <li>Level 13, 2 Elizabeth St, Melbourne Victoria 3000 Australia<br/><br/></li>
+                    <li>Email: John.smith@gmail.com</li>
+                    <li>Phone: +123 456 789 111</li>
+                    <li>Website: www.johnsmith.com</li>
+                </ul>
+            </div>
+            <!-- /Contact Info -->
+
+            <!-- Contact Form -->
+            <div class="contact-form">
+                <h3 class="main-heading"><span>Let's keep in touch</span></h3>
+                <div id="contact-status"></div>
+                <form action="#" id="contactform">
+                    <p>
+                        <label for="name">Your Name</label>
+                        <input type="text" name="name" class="input">
+                    </p>
+                    <p>
+                        <label for="email">Your Email</label>
+                        <input type="text" name="email" class="input">
+                    </p>
+                    <p>
+                        <label for="message">Your Message</label>
+                        <textarea name="message" cols="88" rows="6" class="textarea"></textarea>
+                    </p>
+                    <input type="submit" name="submit" value="Send your message" class="submit">
+                </form>
+            </div>
+            <!-- /Contact Form -->
+        </div>
+        <!-- /contact -->
+
+    </div>
+    <!-- /Content -->
+
+    <!-- Footer -->
+    <div class="footer">
+        <div class="copyright">Copyright © 2012 Balandin Aleksandr</div>
+    </div>
+    <!-- /Footer -->
+
+</div>
+<!-- /Container -->
+<div id="style-switcher">
+    <span class="title">Фон сайта</span>
+    <ul>
+        <li><a class="bg-switcher" value="1" href="#"><img width="20" height="20" src="images/bg1.png"/></a></li>
+        <li><a class="bg-switcher" value="2" href="#"><img width="20" height="20" src="images/bg2.png"/></a></li>
+        <li><a class="bg-switcher" value="3" href="#"><img width="20" height="20" src="images/bg3.png"/></a></li>
+        <li><a class="bg-switcher" value="4" href="#"><img width="20" height="20" src="images/bg4.png"/></a></li>
+        <li><a class="bg-switcher" value="5" href="#"><img width="20" height="20" src="images/bg5.png"/></a></li>
+        <li><a class="bg-switcher" value="6" href="#"><img width="20" height="20" src="images/bg6.png"/></a></li>
+        <li><a class="bg-switcher" value="7" href="#"><img width="20" height="20" src="images/bg7.png"/></a></li>
+        <li><a class="bg-switcher" value="8" href="#"><img width="20" height="20" src="images/bg8.png"/></a></li>
+    </ul>
+</div>
+</body>
+</html>
